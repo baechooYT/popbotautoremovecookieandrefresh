595441e93c2be80881e855177fb759f0f32b2d1e
beachu <66154521+baechooYT@users.noreply.github.com>
2021-09-25 19:52:41 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,18 @@ var event = new KeyboardEvent('keydown', {
 	ctrlKey: true
 });
 
+function getCookie(cookieName){
+    var cookieValue=null;
+    if(document.cookie){
+        var array=document.cookie.split((escape(cookieName)+'='));
+        if(array.length >= 2){
+            var arraySub=array[1].split(';');
+            cookieValue=unescape(arraySub[0]);
+        }
+    }
+    return cookieValue;
+}
+
 setInterval(function(){
 	for (i = 0; i < 100; i++) {
 		getCookie("bot");
